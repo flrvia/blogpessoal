1aa5f981ce1684dca7b5e0df7d130f6d1569435c
flrvia <flavixbatista@gmail.com>
2022-10-20 08:27:00 -0300

    Deploy do Blog Pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
